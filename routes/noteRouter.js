e1c3cd1c6e83f9fd2c3f3fcede5bd0bfe85589c0
xant3r0 <scandiba13@gmail.com>
2026-08-09 12:59:19 +0300

    Wired the error middleware to the note router

--- routes/noteRouter.js
+++ routes/noteRouter.js
@@ -1,12 +1,13 @@
 const Router = require('express');
 const noteController = require('../controllers/noteController.js');
 const authMiddleware = require('../middleware/auth.js');
+const errorMiddleware = require('../middleware/error.js');
 
 const noteRouter = new Router();
 
-noteRouter.get('/notes',authMiddleware,noteController.getNotes);
-noteRouter.post('/notes',authMiddleware,noteController.createNote);
-noteRouter.put('/notes/:id',authMiddleware,noteController.editNote);
-noteRouter.delete('/notes/:id',authMiddleware,noteController.deleteNote);
+noteRouter.get('/notes',authMiddleware,errorMiddleware,noteController.getNotes);
+noteRouter.post('/notes',authMiddleware,errorMiddleware,noteController.createNote);
+noteRouter.put('/notes/:id',authMiddleware,errorMiddleware,noteController.editNote);
+noteRouter.delete('/notes/:id',authMiddleware,errorMiddleware,noteController.deleteNote);
 
-module.exports = noteRouter;
+module.exports = noteRouter; 
