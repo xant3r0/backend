34021c80c8746085e676bdb058f3dbf6e111593a
xant3r0 <scandiba13@gmail.com>
2026-08-09 12:57:31 +0300

    Minor improvement for error middleware

--- middleware/error.js
+++ middleware/error.js
@@ -2,6 +2,10 @@ const errorMiddleware = (err,req,res,next) => {
 
     if(err.statusCode) {
         return res.status(err.statusCode).json({success:false,message:err.message});
+    } else {
+        const err = new Error("Something went wrong, please try again!");
+        err.statusCode = 500;
+        return res.status(err.statusCode).json({success:false,message:err.message});
     };
 
 };
