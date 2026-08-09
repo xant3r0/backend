1b887cd43167b9db8472d4a74e7d021843085045
xant3r0 <scandiba13@gmail.com>
2026-08-09 11:40:47 +0300

    Added the middlewares to the paths

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,12 @@
 const Router = require('express');
 const userController = require('../controllers/userController.js');
+const validationMiddleware = require('../middleware/validation.js');
 
 const userRouter = new Router();
 
 userRouter.post('/login',userController.signIn);
-userRouter.post('/register',userController.signUp);
-userRouter.put('/change-password',userController.changePassword);
+userRouter.post('/register', validationMiddleware,userController.signUp);
+userRouter.put('/change-password',validationMiddleware,userController.changePassword);
 userRouter.delete('/delete-user',userController.deleteUser);
 userRouter.post('/logout',userController.logOut);
 
