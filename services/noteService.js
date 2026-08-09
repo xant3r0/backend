093a671c53b78d3806d1c2a74fcb85d71ddb539e
xant3r0 <scandiba13@gmail.com>
2026-08-09 13:00:00 +0300

    Major refactor of note service layer

--- services/noteService.js
+++ services/noteService.js
@@ -1,43 +1,55 @@
 const db = require('../db.js');
 
 class noteService {
+
+    wrapErr(e) {
+        if(e.statusCode) {
+            throw e;
+        };
+
+        const err = new Error("Something went wrong on the server, please try again later!");
+        err.statusCode = 500;
+        err.orig = e;
+        throw err;
+    };
+
     async getNotes(id) {
         try {
             const notes = await db.query('SELECT * FROM notes WHERE userid = $1',[id]);
 
-            return notes;
+            return notes.rows;
         } catch(e) {
-            throw new Error("Something bad happened! : " + e.message);
+            this.wrapErr(e);
         };
-    };
+    }; 
 
     async createNote(title,contents,id) {
         try {
-            const note = await db.query('INSERT INTO notes (title,contents,createdat,userid) VALUES ($1,$2,NOW(),$3)',[title,contents,id]);
+            const note = await db.query('INSERT INTO notes (title,contents,createdat,userid) VALUES ($1,$2,NOW(),$3) RETURNING createdat',[title,contents,id]);
 
-            return note;
+            return note.rows[0];
         } catch(e) {
-            throw new Error("Something bad happened! : " + e.message);
+            this.wrapErr(e);
         };
     };
 
     async editNote(title,contents,noteid,userid) {
         try {
-            const note = await db.query("UPDATE notes SET title = $1, contents = $2 WHERE noteid = $3 AND userid = $4 RETURNING *",[title,contents,noteid,userid]);
+            const note = await db.query("UPDATE notes SET title = $1, contents = $2 WHERE noteid = $3 AND userid = $4 RETURNING noteid",[title,contents,noteid,userid]);
 
-            return note;
+            return note.rows[0];
         } catch(e) {
-            throw new Error("Something bad happened! : " + e.message);
+            this.wrapErr(e);
         };
     };
 
     async deleteNote(noteid,userid) {
         try {
-            const note = await db.query("DELETE FROM notes WHERE noteid = $1 and userid = $2 RETURNING *",[noteid,userid]);
+            const note = await db.query("DELETE FROM notes WHERE noteid = $1 and userid = $2 RETURNING noteid",[noteid,userid]);
 
-            return note;
+            return note.rows[0];
         } catch(e) {
-            throw new Error("Something bad happened! : " + e.message);
+            this.wrapErr(e);
         };
     };
 };
