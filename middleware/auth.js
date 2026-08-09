3966b6bb80a207c6e706c6ef6be9f4ad0ae1b158
xant3r0 <scandiba13@gmail.com>
2026-08-09 11:35:25 +0300

    Added auth middleware(checks if a user is logged in)

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const authMiddleware = (req,res,next) => {
     if(!req.session || !req.session.userId) {
         return res.status(401).json({message: "Please login in your account!"});
     } else {
-        next();
+        return next();
     };
 
 };
